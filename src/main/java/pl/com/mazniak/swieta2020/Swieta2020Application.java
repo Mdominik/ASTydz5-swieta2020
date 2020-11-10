c238164c8cda47d3f80c9c5f7f75660d8ef78702
Mdominik <mazniakd@gmail.com>
2020-11-10 18:57:15 +0100

    Initial commit

---
+++ src/main/java/pl/com/mazniak/swieta2020/Swieta2020Application.java
@@ -0,0 +1,13 @@
+package pl.com.mazniak.swieta2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swieta2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swieta2020Application.class, args);
+    }
+
+}
